ebf37ab06884e5ea45c27a5eb985fce8465710fe
unknown <ealdunn99@gmail.com>
2020-05-20 19:50:04 +0400

    mp3

--- app.js
+++ app.js
@@ -2,20 +2,46 @@ window.addEventListener('load', yuklendi());
 
 
 function yuklendi() {
-    //  let ses = document.querySelector('#player');
-    let ses = new Audio();
-
-    ses.src="mp3/1.pm3"
+    let songs = ["mp3/1.mp3", "mp3/2.mp3", "mp3/3.mp3", "mp3/4.mp3"]
+    let song = new Audio();
+    let currentSong = 0
+    song.src = songs[currentSong]
 
 
 
 
     let btn = document.querySelector('#playSound');
     btn.addEventListener('click', function () {
-        ses.play();
+        if (song.paused) {
+            song.play()
+            btn.textContent = "Pause"
+        }
+        else {
+            song.pause()
+            btn.textContent = "Play"
+        }
     });
-    let pauseBtn = document.querySelector('#pauseSound');
-    pauseBtn.addEventListener('click', function () {
-        ses.pause();
+
+
+    let nextSound = document.querySelector('#NextSound');
+    nextSound.addEventListener('click', function () {
+        currentSong++
+        if (currentSong == 4) {
+            currentSong = 0
+        }
+        song.src = songs[currentSong]
+        song.play()
     });
+
+    let prevSound = document.querySelector('#prevSound');
+    prevSound.addEventListener('click', function () {
+        currentSong--
+        if (currentSong < 0 ) {
+            currentSong = 3
+        }
+        song.src = songs[currentSong]
+        song.play()
+    });
+
+   
 }
